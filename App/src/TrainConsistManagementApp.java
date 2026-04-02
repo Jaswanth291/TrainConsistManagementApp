713867215bfd9011cdc567453befd5b10b96fc39
Jaswanth291 <gaddamjaswanth550@gmail.com>
2026-04-02 16:34:51 +0530

    UC4 - Maintain Ordered Bogie IDs (TreeSet & SortedSet)

--- App/src/TrainConsistManagementApp.java
+++ App/src/TrainConsistManagementApp.java
@@ -16,30 +16,29 @@ class Bogie {
     }
 }
 
-// Train class using HashSet
+// Train class using TreeSet (SortedSet)
 class Train {
     String trainName;
-    HashSet<String> bogieIds; // stores unique IDs
+    SortedSet<String> bogieIds; // automatically sorted
     ArrayList<Bogie> bogies;
 
     Train(String trainName) {
         this.trainName = trainName;
-        bogieIds = new HashSet<>();
+        bogieIds = new TreeSet<>(); // TreeSet = SortedSet
         bogies = new ArrayList<>();
     }
 
-    // Add bogie only if ID is unique
+    // Add bogie (unique + sorted)
     void addBogie(Bogie b) {
-        if (bogieIds.contains(b.bogieId)) {
-            System.out.println("Duplicate Bogie ID not allowed: " + b.bogieId);
-        } else {
-            bogieIds.add(b.bogieId);
+        if (bogieIds.add(b.bogieId)) { // add returns false if duplicate
             bogies.add(b);
             System.out.println("Bogie added: " + b);
+        } else {
+            System.out.println("Duplicate Bogie ID not allowed: " + b.bogieId);
         }
     }
 
-    // Display bogies
+    // Display all bogies
     void displayBogies() {
         System.out.println("\nTrain: " + trainName);
         for (Bogie b : bogies) {
@@ -47,27 +46,37 @@ class Train {
         }
     }
 
-    // Display unique IDs
-    void displayUniqueIDs() {
-        System.out.println("\nUnique Bogie IDs: " + bogieIds);
+    // Display sorted bogie IDs
+    void displaySortedIDs() {
+        System.out.println("\nSorted Bogie IDs: " + bogieIds);
+    }
+
+    // Show subset (range)
+    void displayRange(String from, String to) {
+        System.out.println("\nRange (" + from + " to " + to + "): " +
+                ((TreeSet<String>) bogieIds).subSet(from, to));
     }
 }
 
 // Main class
-public class TrainConsistManagementApp {
+public class UC4MaintainOrderedBogieIDs {
 
     public static void main(String[] args) {
 
         Train train = new Train("Chennai Express");
 
-        train.addBogie(new Bogie("S1", "Sleeper"));
         train.addBogie(new Bogie("S2", "Sleeper"));
         train.addBogie(new Bogie("A1", "AC"));
+        train.addBogie(new Bogie("S1", "Sleeper"));
+        train.addBogie(new Bogie("G1", "General"));
 
-        // Duplicate attempt
+        // Duplicate test
         train.addBogie(new Bogie("S1", "Sleeper"));
 
         train.displayBogies();
-        train.displayUniqueIDs();
+        train.displaySortedIDs();
+
+        // Range query
+        train.displayRange("A1", "S2");
     }
 }
